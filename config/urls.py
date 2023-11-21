59e15c78bf5f03cfbb3cbef6bc5098c20b3fcb2e
Bin Nguyễn <tu.nguyen@risotech.vn>
2023-11-21 09:30:35 +0700

    add grappelli

--- config/urls.py
+++ config/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path("about/", TemplateView.as_view(template_name="pages/about.html"), name="about"),
     # Django Admin, use {% url 'admin:index' %}
+    path("grappelli/", include("one.grappelli.urls")),
     path(settings.ADMIN_URL, admin.site.urls),
     # User management
     path("users/", include("one.users.urls", namespace="users")),
