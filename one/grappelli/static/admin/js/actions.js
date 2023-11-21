9c29b25405237b66734edcded812b209e4e58a48
Ly Quang Thai <thai.ly@masterisehomes.com>
2023-11-21 10:50:18 +0700

    fix file one/grappelli/static/admin/js/actions.js

--- one/grappelli/static/admin/js/actions.js
+++ one/grappelli/static/admin/js/actions.js
@@ -1,62 +1,62 @@
 /*global gettext, interpolate, ngettext*/
 'use strict';
 {
-    function show(selector) {
-        document.querySelectorAll(selector).forEach(function(el) {
+    const show = function (selector) {
+        document.querySelectorAll(selector).forEach(function (el) {
             el.classList.remove('hidden');
         });
-    }
+    };
 
-    function hide(selector) {
-        document.querySelectorAll(selector).forEach(function(el) {
+    const hide = function (selector) {
+        document.querySelectorAll(selector).forEach(function (el) {
             el.classList.add('hidden');
         });
-    }
+    };
 
-    function showQuestion(options) {
+    const showQuestion = function (options) {
         hide(options.acrossClears);
         show(options.acrossQuestions);
         hide(options.allContainer);
-    }
+    };
 
-    function showClear(options) {
+    const showClear = function (options) {
         show(options.acrossClears);
         hide(options.acrossQuestions);
         document.querySelector(options.actionContainer).classList.remove(options.selectedClass);
         show(options.allContainer);
         hide(options.counterContainer);
-    }
+    };
 
-    function reset(options) {
+    const reset = options => {
         hide(options.acrossClears);
         hide(options.acrossQuestions);
         hide(options.allContainer);
         show(options.counterContainer);
-    }
+    };
 
-    function clearAcross(options) {
+    const clearAcross = options => {
         reset(options);
         const acrossInputs = document.querySelectorAll(options.acrossInput);
-        acrossInputs.forEach(function(acrossInput) {
+        acrossInputs.forEach(function (acrossInput) {
             acrossInput.value = 0;
         });
         document.querySelector(options.actionContainer).classList.remove(options.selectedClass);
-    }
+    };
 
-    function checker(actionCheckboxes, options, checked) {
+    const checker = (actionCheckboxes, options, checked) => {
         if (checked) {
             showQuestion(options);
         } else {
             reset(options);
         }
-        actionCheckboxes.forEach(function(el) {
+        actionCheckboxes.forEach(function (el) {
             el.checked = checked;
             el.closest('tr').classList.toggle(options.selectedClass, checked);
         });
-    }
+    };
 
-    function updateCounter(actionCheckboxes, options) {
-        const sel = Array.from(actionCheckboxes).filter(function(el) {
+    const updateCounter = (actionCheckboxes, options) => {
+        const sel = Array.from(actionCheckboxes).filter(function (el) {
             return el.checked;
         }).length;
         const counter = document.querySelector(options.counterContainer);
@@ -77,7 +77,7 @@
         } else {
             clearAcross(options);
         }
-    }
+    };
 
     // GRAPPELLI CUSTOM: changed classes, added class for counterSpan
     const defaults = {
@@ -92,7 +92,7 @@
         selectedClass: "grp-selected"
     };
 
-    window.Actions = function(actionCheckboxes, options) {
+    window.Actions = function (actionCheckboxes, options) {
         options = Object.assign({}, defaults, options);
         let list_editable_changed = false;
         let lastChecked = null;
@@ -106,24 +106,24 @@
             shiftPressed = event.shiftKey;
         });
 
-        document.getElementById(options.allToggleId).addEventListener('click', function(event) {
+        document.getElementById(options.allToggleId).addEventListener('click', function (event) {
             checker(actionCheckboxes, options, this.checked);
             updateCounter(actionCheckboxes, options);
         });
 
-        document.querySelectorAll(options.acrossQuestions + " a").forEach(function(el) {
-            el.addEventListener('click', function(event) {
+        document.querySelectorAll(options.acrossQuestions + " a").forEach(function (el) {
+            el.addEventListener('click', function (event) {
                 event.preventDefault();
                 const acrossInputs = document.querySelectorAll(options.acrossInput);
-                acrossInputs.forEach(function(acrossInput) {
+                acrossInputs.forEach(function (acrossInput) {
                     acrossInput.value = 1;
                 });
                 showClear(options);
             });
         });
 
-        document.querySelectorAll(options.acrossClears + " a").forEach(function(el) {
-            el.addEventListener('click', function(event) {
+        document.querySelectorAll(options.acrossClears + " a").forEach(function (el) {
+            el.addEventListener('click', function (event) {
                 event.preventDefault();
                 document.getElementById(options.allToggleId).checked = false;
                 clearAcross(options);
@@ -146,8 +146,8 @@
             return filtered;
         };
 
-        Array.from(document.getElementById('result_list').tBodies).forEach(function(el) {
-            el.addEventListener('change', function(event) {
+        Array.from(document.getElementById('result_list').tBodies).forEach(function (el) {
+            el.addEventListener('change', function (event) {
                 const target = event.target;
                 if (target.classList.contains('action-select')) {
                     const checkboxes = affectedCheckboxes(target, shiftPressed);
@@ -161,10 +161,11 @@
         });
 
         // GRAPPELLI CUSTOM: changed class
-        document.querySelector('#grp-changelist-form button[name=index]').addEventListener('click', function() {
+        document.querySelector('#grp-changelist-form button[name=index]').addEventListener('click', function () {
             if (list_editable_changed) {
                 const confirmed = confirm(gettext("You have unsaved changes on individual editable fields. If you run an action, your unsaved changes will be lost."));
                 if (!confirmed) {
+                    /** global: event */
                     event.preventDefault();
                 }
             }
@@ -174,7 +175,7 @@
         const el = document.querySelector('#grp-changelist-form input[name=_save]');
         // The button does not exist if no fields are editable.
         if (el) {
-            el.addEventListener('click', function(event) {
+            el.addEventListener('click', function (event) {
                 if (document.querySelector('[name=action]').value) {
                     const text = list_editable_changed
                         ? gettext("You have selected an action, but you haven’t saved your changes to individual fields yet. Please click OK to save. You’ll need to re-run the action.")
@@ -190,15 +191,15 @@
     // Call function fn when the DOM is loaded and ready. If it is already
     // loaded, call the function now.
     // http://youmightnotneedjquery.com/#ready
-    function ready(fn) {
+    const ready = fn => {
         if (document.readyState !== 'loading') {
             fn();
         } else {
             document.addEventListener('DOMContentLoaded', fn);
         }
-    }
+    };
 
-    ready(function() {
+    ready(function () {
         const actionsEls = document.querySelectorAll('tr input.action-select');
         if (actionsEls.length > 0) {
             Actions(actionsEls);
