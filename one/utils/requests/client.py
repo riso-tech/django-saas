3eb2cede2f8dfc98dbabb65d47092d166b30f1d9
riso-tech <80811037+riso-tech@users.noreply.github.com>
2023-11-22 22:14:16 +0700

    add custom client (#9)
    * add custom client
    
    * add custom client
    
    ---------
    
    Co-authored-by: Bin Nguyễn <tu.nguyen@risotech.vn>

---
+++ one/utils/requests/client.py
@@ -0,0 +1,42 @@
+import time
+
+import requests
+from requests import Request
+
+from .exceptions import ClientTimeoutError, ServiceRequestError
+
+
+class ServiceClient:
+    def __init__(self, retries=3, timeout=10):
+        self.retries = retries
+        self.timeout = timeout
+        self.session = requests.session()
+
+    def _request_loop(self, http_method, endpoint, data=None, params=None, files=None, json=None, retries=None):
+        retry: int = 0
+        retries = self.retries if retries is None else retries
+        while retry <= retries:
+            try:
+                return self.request(http_method, endpoint, data, params, files, json)
+            except Exception as err:
+                if retry < retries and type(err) is ClientTimeoutError:
+                    retry += 1
+                    time.sleep(retry)
+                    continue
+                else:
+                    raise err
+
+    def request(self, http_method, endpoint, data=None, params=None, files=None, json=None):
+        request = Request(method=http_method, url=endpoint, data=data, params=params, files=files, json=json)
+        prepared_request = request.prepare()
+        try:
+            return self.session.send(request=prepared_request, timeout=self.timeout)
+        except (
+            requests.exceptions.ConnectTimeout,
+            requests.exceptions.Timeout,
+            requests.exceptions.ReadTimeout,
+            requests.exceptions.ConnectionError,
+        ):
+            raise ClientTimeoutError()
+        except Exception:
+            raise ServiceRequestError()
